13b8093c4a6d49ce4ae0d101a8293f13ec392ae1
seongmoolee <seongmoolee@gmail.com>
2024-02-20 02:47:54 +0900

    deploy

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+APPNAME="token_37162313"
+
+ENV_FILE=".env"
+PRIVATE_KEY=$(grep "PRIVATE_KEY" $ENV_FILE | cut -d '=' -f2)
+
+QUERY_URL="https://api.explorer.aleo.org/v1"
+BROADCAST_URL="${QUERY_URL}/testnet3/transaction/broadcast"
+
+snarkos developer deploy "${APPNAME}.aleo" \
+  --path "./build/" \
+  --private-key "${PRIVATE_KEY}" \
+  --query "${QUERY_URL}" \
+  --priority-fee 3000000 \
+  --broadcast "${BROADCAST_URL}" \
